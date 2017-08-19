cad003b9773fe59d641e0ea64e8df20a463cd5cc
agoalofalife <agoalofalife@gmail.com>
2017-08-19 21:14:19 +0300

    some code

--- routes.go
+++ routes.go
@@ -2,10 +2,14 @@ package ethereal
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
 	router := gin.Default()
+	router.
 	v1 := router.Group("/api/v1/todos")
 	{
 		v1.POST("/", CreateTodo)
@@ -15,4 +19,14 @@ func main() {
 		v1.DELETE("/:id", DeleteTodo)
 	}
 	router.Run()
+}
+
+func envLoading() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	//s3Bucket := os.Getenv("S3_BUCKET")
+	//secretKey := os.Getenv("SECRET_KEY")
 }
