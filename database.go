8c85e6ccd18c08e6b8064f50e403741316947180
agoalofalife <agoalofalife@gmail.com>
2017-08-19 21:57:45 +0300

    add pre

--- database.go
+++ database.go
@@ -3,11 +3,14 @@ package ethereal
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 )
 
 func Database() *gorm.DB {
 	//open a db connection
-	db, err := gorm.Open("mysql", "root:12345@/demo?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", os.Getenv("MYSQL_LOGIN")+
+		":"+os.Getenv("MYSQL_PASSWORD")+
+		"@/"+os.Getenv("MYSQL_DATABASE")+"?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect database")
 	}
--- routes.go
+++ routes.go
@@ -2,21 +2,30 @@ package ethereal
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"log"
-	"os"
+	"net/http"
+	"strconv"
 )
 
-func main() {
+type App struct {
+	Db *gorm.DB
+}
+
+func Run() {
+	envLoading()
 	router := gin.Default()
-	router.
+	Database()
+	//app := App{Db: Database()}
+
 	v1 := router.Group("/api/v1/todos")
 	{
 		v1.POST("/", CreateTodo)
-		v1.GET("/", FetchAllTodo)
-		v1.GET("/:id", FetchSingleTodo)
-		v1.PUT("/:id", UpdateTodo)
-		v1.DELETE("/:id", DeleteTodo)
+		//v1.GET("/", FetchAllTodo)
+		//v1.GET("/:id", FetchSingleTodo)
+		//v1.PUT("/:id", UpdateTodo)
+		//v1.DELETE("/:id", DeleteTodo)
 	}
 	router.Run()
 }
@@ -29,4 +38,12 @@ func envLoading() {
 
 	//s3Bucket := os.Getenv("S3_BUCKET")
 	//secretKey := os.Getenv("SECRET_KEY")
+}
+
+func CreateTodo(c *gin.Context) {
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	todo := Todo{Title: c.PostForm("title"), Completed: completed}
+	db := Database()
+	db.Save(&todo)
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID})
 }
