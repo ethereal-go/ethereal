de4a222caa55bd3376dc09c54f349413ffe340b8
agoalofalife <agoalofalife@gmail.com>
2017-08-20 14:39:37 +0300

    rollback migration

--- cli.go
+++ cli.go
@@ -5,12 +5,13 @@ import (
 )
 
 func CliRun() {
-
 	database := flag.String("database", "migrate", "action database")
 	flag.Parse()
 
 	switch *database {
 	case "migrate":
 		app.Db.AutoMigrate(&Todo{})
+	case "rollback":
+		app.Db.DropTable(&Todo{})
 	}
 }
