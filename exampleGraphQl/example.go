ece68704b3ad7e83880981dbd8dd83635ddb627f
agoalofalife <agoalofalife@gmail.com>
2017-08-23 22:44:30 +0300

    some

--- exampleGraphQl/example.go
+++ exampleGraphQl/example.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/graphql/testutil"
 	"github.com/graphql-go/handler"
 	"math/rand"
 	"net/http"
@@ -205,13 +204,13 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 	return result
 }
 
-func main() {
+func Start() {
 	//http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 	//	result := executeQuery(r.URL.Query().Get("query"), schema)
 	//	json.NewEncoder(w).Encode(result)
 	//})
 	h := handler.New(&handler.Config{
-		Schema: &testutil.StarWarsSchema,
+		Schema: &schema,
 		Pretty: true,
 	})
 	http.Handle("/graphql", h)
@@ -219,12 +218,12 @@ func main() {
 	fs := http.FileServer(http.Dir("../static"))
 	http.Handle("/", fs)
 	// Display some basic instructions
-	fmt.Println("Now server is running on port 8080")
-	fmt.Println("Get single todo: curl -g 'http://localhost:8080/graphql?query={todo(id:\"b\"){id,text,done}}'")
-	fmt.Println("Create new todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:\"My+new+todo\"){id,text,done}}'")
-	fmt.Println("Update todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{updateTodo(id:\"a\",done:true){id,text,done}}'")
-	fmt.Println("Load todo list: curl -g 'http://localhost:8080/graphql?query={todoList{id,text,done}}'")
-	fmt.Println("Access the web app via browser at 'http://localhost:8080'")
+	//fmt.Println("Now server is running on port 8080")
+	//fmt.Println("Get single todo: curl -g 'http://localhost:8080/graphql?query={todo(id:\"b\"){id,text,done}}'")
+	//fmt.Println("Create new todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:\"My+new+todo\"){id,text,done}}'")
+	//fmt.Println("Update todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{updateTodo(id:\"a\",done:true){id,text,done}}'")
+	//fmt.Println("Load todo list: curl -g 'http://localhost:8080/graphql?query={todoList{id,text,done}}'")
+	//fmt.Println("Access the web app via browser at 'http://localhost:8080'")
 
 	http.ListenAndServe(":8080", nil)
 }
