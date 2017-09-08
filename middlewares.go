7c851257cbda729c3de6c9534f797b6bd5c4432a
agoalofalife <agoalofalife@gmail.com>
2017-09-08 10:16:03 +0300

    remove old code

--- middlewares.go
+++ middlewares.go
@@ -33,48 +33,6 @@ func (m Middleware) LoadApplication(application *Application) []alice.Constructo
 	return m.includeMiddleware
 }
 
-/**
-/ ability to set jwt token all queries or choose query
-*/
-type MiddlewareJWTToken struct {
-	jwt            EtherealClaims
-	StatusError    int
-	ResponseError  string
-	authenticated  bool
-	responseWriter http.ResponseWriter
-	included       bool // flag is enabled or disabled authJwtToken
-}
-
-func (m MiddlewareJWTToken) Add(where *[]alice.Constructor, application *Application) {
-	confToken := GetCnf("AUTH.JWT_TOKEN").(string)
-
-	if confToken == "local" {
-		m.included = true
-		*where = append(*where, func(handler http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				m.responseWriter = w
-				if check, err := m.jwt.Verify(r); !check {
-					m.ResponseError = handlerErrorToken(err).Error()
-				} else {
-					m.authenticated = true
-				}
-				CtxStruct(application, m)
-				handler.ServeHTTP(w, r)
-			})
-		})
-	} else if confToken == "global" {
-		// check jwt token all queries..
-		*where = append(*where, func(handler http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				if check, err := m.jwt.Verify(r); !check {
-					json.NewEncoder(w).Encode(handlerErrorToken(err).Error())
-					w.WriteHeader(m.StatusError)
-				}
-			})
-		})
-	}
-}
-
 // ---- waiting for your implementation ------
 
 // middleware set Accept-Language
