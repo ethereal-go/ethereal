7f300b075857aa083223c6deb4fb584f1637893b
agoalofalife <agoalofalife@gmail.com>
2017-09-11 13:13:30 +0300

    add function in utils copy file

--- utils/file.go
+++ utils/file.go
@@ -1,6 +1,9 @@
 package utils
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 func FileExists(name string) bool {
 	if _, err := os.Stat(name); err != nil {
@@ -11,9 +14,27 @@ func FileExists(name string) bool {
 	return true
 }
 
-func DirExist(path string)  bool  {
+func DirExist(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return false
 	}
 	return true
+}
+
+func Copy(dst, source string) error {
+	s, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	return d.Close()
 }
