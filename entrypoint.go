a6214b49739bf7059b95e8c3193c68ed2db3008e
agoalofalife <agoalofalife@gmail.com>
2017-09-11 14:25:26 +0300

    remove two mode just move cli separate package

--- entrypoint.go
+++ entrypoint.go
@@ -37,71 +37,63 @@ func Start() {
 		debug string = GetCnf("GRAPHQL.DEBUG").(string)
 		host  string = GetCnf("HOST.PORT").(string)
 	)
-	// First we have to determine the mode of operation
-	// - cli console
-	// - api server
-	// Secondly, we must determine the sequence of actions
 
-	if len(os.Args) > 1 {
-		//CliRun()
-		CliExecute()
-	} else {
-
-		App = Application{
-			Db:              ConstructorDb(),
-			I18n:            ConstructorI18N(),
-			Middleware:      ConstructorMiddleware(),
-			GraphQlQuery:    startQueries(),
-			GraphQlMutation: startMutations(),
-			Context:         context.Background(),
-			Config:          ConstructorConfig(),
-		}
-		// link itself
-		CtxStruct(&App, App)
-		App.Middleware.LoadApplication(&App)
-		//root mutation
-		var rootMutation = graphql.NewObject(graphql.ObjectConfig{
-			Name:   "RootMutation",
-			Fields: App.GraphQlMutation,
-		})
-
-		// root query
-		var rootQuery = graphql.NewObject(graphql.ObjectConfig{
-			Name:   "RootQuery",
-			Fields: App.GraphQlQuery,
-		})
-
-		// define schema, with our rootQuery and rootMutation
-		var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
-			Query:    rootQuery,
-			Mutation: rootMutation,
-		})
-		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			opts := handler.NewRequestOptions(r)
-			result := graphql.Do(graphql.Params{
-				Schema:         schema,
-				OperationName:  opts.OperationName,
-				VariableValues: opts.Variables,
-				RequestString:  opts.Query,
-				Context:        App.Context,
-			})
-			if len(result.Errors) > 0 {
-				log.Printf("wrong result, unexpected errors: %v", result.Errors)
-				return
-			}
-			json.NewEncoder(w).Encode(result)
-		})
-
-		// here can add middleware
-		http.Handle("/graphql", alice.New(App.Middleware.includeMiddleware...).Then(h))
-
-		if debug == "true" {
-			_, filename, _, _ := runtime.Caller(0)
-			fs := http.FileServer(http.Dir(path.Dir(filename) + "/static"))
-			http.Handle("/", fs)
-		}
-
-		fmt.Println(runServer + host)
-		http.ListenAndServe(":"+host, nil)
+	App = Application{
+		Db:              ConstructorDb(),
+		I18n:            ConstructorI18N(),
+		Middleware:      ConstructorMiddleware(),
+		GraphQlQuery:    startQueries(),
+		GraphQlMutation: startMutations(),
+		Context:         context.Background(),
+		Config:          ConstructorConfig(),
 	}
+	// link itself
+	CtxStruct(&App, App)
+	App.Middleware.LoadApplication(&App)
+
+	//root mutation
+	var rootMutation = graphql.NewObject(graphql.ObjectConfig{
+		Name:   "RootMutation",
+		Fields: App.GraphQlMutation,
+	})
+
+	// root query
+	var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+		Name:   "RootQuery",
+		Fields: App.GraphQlQuery,
+	})
+
+	// define schema, with our rootQuery and rootMutation
+	var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+		Query:    rootQuery,
+		Mutation: rootMutation,
+	})
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		opts := handler.NewRequestOptions(r)
+		result := graphql.Do(graphql.Params{
+			Schema:         schema,
+			OperationName:  opts.OperationName,
+			VariableValues: opts.Variables,
+			RequestString:  opts.Query,
+			Context:        App.Context,
+		})
+		if len(result.Errors) > 0 {
+			log.Printf("wrong result, unexpected errors: %v", result.Errors)
+			return
+		}
+		json.NewEncoder(w).Encode(result)
+	})
+
+	// here can add middleware
+	http.Handle("/graphql", alice.New(App.Middleware.includeMiddleware...).Then(h))
+
+	if debug == "true" {
+		_, filename, _, _ := runtime.Caller(0)
+		fs := http.FileServer(http.Dir(path.Dir(filename) + "/static"))
+		http.Handle("/", fs)
+	}
+
+	fmt.Println(runServer + host)
+	http.ListenAndServe(":"+host, nil)
+
 }
