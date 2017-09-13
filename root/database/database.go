31d09e128dcff5ddfc72ae5a4fcf2b3f700d0925
agoalofalife <agoalofalife@gmail.com>
2017-09-13 10:41:17 +0300

    add factory type database

--- root/database/database.go
+++ root/database/database.go
@@ -1,8 +1,26 @@
 package database
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+)
 
 type DatabaseConnector interface {
 	Connection() *gorm.DB
 	Parse() DatabaseConnector
+}
+
+func FactoryDatabase(typeDB string) (DatabaseConnector, error) {
+	switch typeDB {
+	case "mysql":
+		return DatabaseMysql{}, nil
+	case "postgres":
+		return DatabasePostgres{}, nil
+	case "sqlite3":
+		return DatabasePostgres{}, nil
+	case "sqlServer":
+		return SQLServer{}, nil
+	default:
+		return nil, errors.New("You have not selected a database type.")
+	}
 }
